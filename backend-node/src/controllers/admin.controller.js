9ac98c35ee0c8767ba035ed48d95eca92a370c8a
Cypher2801 <147578076+Cypher2801@users.noreply.github.com>
2025-06-12 08:37:24 +0530

    minor error fix

--- backend-node/src/controllers/admin.controller.js
+++ backend-node/src/controllers/admin.controller.js
@@ -63,13 +63,6 @@ export const getStatsByRegion = asyncHandler(async (req, res) => {
     }, "Region stats retrieved successfully"));
 });
 
-import mongoose from "mongoose";
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
-import { User } from "../models/user.model.js";
-import { Transaction } from "../models/transaction.model.js";
-
 export const getStatsByUser = asyncHandler(async (req, res) => {
     const userId = req.params.id;
 
